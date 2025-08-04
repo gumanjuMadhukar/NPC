214818f38b070f22d966f1698947a8d8d547fe14
Your Name <aaratirai777@gmail,com>
2025-08-04 14:17:16 +0545

    push after the pull form the develop

---
+++ app/Http/Requests/OfficeAdmin/Applicant/SlcRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\OfficeAdmin\Applicant;
+
+use App\Http\Requests\RestRequest;
+
+class SlcRequest extends RestRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'sometimes',
+            'level_id' => 'required',
+            'school_name' =>  'required',
+            'board_university' => 'required',
+            'passed_year' => 'required|integer',
+            'transcript_image' => 'required',
+            'transcript_bac_1' => 'sometimes',
+            'provisional_image' => 'required',
+            'character_image' => 'required',
+            'equivalence_certificate' => 'sometimes',
+        ];
+    }
+}
